db5699b955c8ca6a7a0fe41ec0616d840bd99bd2
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-02-20 14:57:43 +0000

    mvp complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [toDoList, setTodo] = useState([
+  ])
+
+
+  const[newToDo, setNewToDo] = useState('')
+
+
+  const completeToDo = (index) => {
+    const copyToDo = [...toDoList]
+    const updatedToDo = {...copyToDo[index]}
+    updatedToDo.completed = true
+    copyToDo[index] = updatedToDo
+    setTodo(copyToDo)
+  }
+
+
+  const toDoNodes = toDoList.map((todo, index) => {
+    return (
+      <li key={index}>{todo.name}
+      {todo.completed ? <span>Completed!</span> :
+      <button onClick={()=>completeToDo(index)}>Complete</button>}</li> // REMEBER TO ADD BUTTON FUNCTION
+    )
+  })
+
+
+  const handleToDoInput = (event) => {
+    setNewToDo(event.target.value)
+  }
+
+
+  const saveNewToDO = (event) => {
+    event.preventDefault()
+    const copyToDo = [...toDoList];
+    copyToDo.push({name:newToDo, completed:false})
+    setTodo(copyToDo)
+    setNewToDo('')
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 htmlId='heading'>ToDo List</h1>
+      <ul>
+
+      </ul>
+        {toDoNodes}
+
+    <form onSubmit={saveNewToDO}>
+      <label htmlFor='new-todo'>Add new ToDo:</label>
+      <input id='new-todo' type='text' value={newToDo} onChange={handleToDoInput}/>
+      <input type='submit' value='Save New ToDo'></input>
+    </form>
+
+  </div>
   );
 }
 
