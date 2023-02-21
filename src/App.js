85e8cba3938b08717f222129bb838e8f0626c0a9
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-02-21 09:06:14 +0000

    radio buttons working

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 
 
   const[newToDo, setNewToDo] = useState('')
+  const[priority, setPriority] = useState('')
 
 
   const completeToDo = (index) => {
@@ -22,10 +23,10 @@ function App() {
 
   const toDoNodes = toDoList.map((todo, index) => {
     return (
-      <li key={index}>{todo.name}
+      <li id='list-item' key={index}>{todo.name}
       <p>{todo.priority}</p>
       {todo.completed ? <span>Completed!</span> :
-      <button onClick={()=>completeToDo(index)}>Complete</button>}</li> 
+      <button onClick={()=>completeToDo(index)} htmlId='complete-button'>Complete</button>}</li> 
     )
   })
 
@@ -34,12 +35,19 @@ function App() {
     setNewToDo(event.target.value)
   }
 
+  const setHigh = () => {
+    setPriority('High')
+  }
+  const setLow = () => {
+    setPriority('Low')
+  }
+
 
 
   const saveNewToDO = (event) => {
     event.preventDefault()
     const copyToDo = [...toDoList];
-    copyToDo.push({name:newToDo, priority:newToDo.priority, completed:false})
+    copyToDo.push({name:newToDo, priority:priority, completed:false})
     setTodo(copyToDo)
     setNewToDo('')
   }
@@ -50,22 +58,25 @@ function App() {
     <div className="App">
       <h1 id='heading'>ToDo List</h1>
       <ul>
-
+        <div id='todo'>
+      {toDoNodes}
+      </div>
       </ul>
-        {toDoNodes}
 
-    <form onSubmit={saveNewToDO}>
+    <form id='form' onSubmit={saveNewToDO}>
+      <div id='form-box'>
       <label htmlFor='new-todo'>Add new ToDo:</label>
       <input id='new-todo' type='text' value={newToDo} onChange={handleToDoInput} required/>
 
       <div id='priority'>
         <label>Priority </label>
         <label for='high'>High</label>
-        <input type='radio' name='priority' id='high' value='high' required/>
+        <input type='radio' name='priority' id='high' value='high' onChange={setHigh} required/>
         <label for='high'>low</label>
-        <input type='radio' name='priority' id='low' value='low' required/>
+        <input type='radio' name='priority' id='low' value='low' onChange={setLow} required/>
       </div>
       <input type='submit' value='Save New ToDo'></input>
+      </div>
     </form>
 
   </div>
